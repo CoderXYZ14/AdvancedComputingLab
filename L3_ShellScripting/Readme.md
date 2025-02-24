4330f03b3e15be3f9e5dad545b6edf9a443a7cbe
CoderXYZ14 <s1404islam@gmail.com>
2025-02-24 14:34:28 +0530

    Readme updated

--- L3_ShellScripting/Readme.md
+++ L3_ShellScripting/Readme.md
@@ -1,24 +1,36 @@
-# Create all needed folders
+# Commands for File Management Script
 
+## Create all needed folders
+
+```bash
 mkdir sourcefolder
 mkdir folder1
 mkdir folder2
 mkdir destination
+```
 
-# Create test files
+## Create test files
 
+```bash
 echo "test" > sourcefolder/abc.txt
 echo "test" > folder1/xyz.dat
 echo "test" > folder2/xyz.dat
+```
 
-# Make script executable
+## Make script executable
 
+```bash
 chmod +x myp.sh
+```
 
-# Run Functionality 1 (add date to files)
+## Run Functionality 1 (add date to files)
 
+```bash
 ./myp.sh -1 sourcefolder
+```
 
-# Run Functionality 2 (copy common files)
+## Run Functionality 2 (copy common files)
 
+```bash
 ./myp.sh -2 folder1 folder2 destination
+```
--- L3_ShellScripting/myp.sh
+++ L3_ShellScripting/myp.sh
@@ -1,51 +1,31 @@
 #!/bin/bash
-# This is a simple script for file operations
 
-# If user types -1, this function will run
 function add_date_to_files() {
-    # $1 means first argument after -1
     folder=$1
     
-    # Go through each file in the folder
     for file in $folder/*; do
-        # Get just the file name
         name=$(basename "$file")
-        # Get file extension
         ext="${name##*.}"
-        # Get name without extension
         base="${name%.*}"
-        # Get date (in format DDMMYYYY)
         date=$(date +%d%m%Y)
-        
-        # Create new name
         new_name="${base}${date}.${ext}"
-        
-        # Rename file
         mv "$file" "$folder/$new_name"
         echo "Changed $name to $new_name"
     done
 }
 
-# If user types -2, this function will run
 function copy_common_files() {
-    # Get the folders from arguments
     folder1=$1
     folder2=$2
     dest=$3
     
-    # Make destination folder if it doesn't exist
     mkdir -p $dest
     
-    # Look for common files
     for file in $folder1/*; do
         name=$(basename "$file")
-        # Check if same file exists in folder2
         if [ -f "$folder2/$name" ]; then
-            # Get name and extension
             ext="${name##*.}"
             base="${name%.*}"
-            
-            # Copy files with new names
             cp "$folder1/$name" "$dest/${base}1.${ext}"
             cp "$folder2/$name" "$dest/${base}2.${ext}"
             echo "Copied $name from both folders"
@@ -53,7 +33,6 @@ function copy_common_files() {
     done
 }
 
-# Main part of script
 if [ "$1" = "-1" ]; then
     add_date_to_files $2
 elif [ "$1" = "-2" ]; then
